6df5b7a584e8323b1335e0a84803bb59949c5f5a
Libranner <libranner@gmail.com>
2020-11-14 16:17:52 +0100

    Initial Commit

---
+++ SwiftBooks/SwiftBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBooksApp.swift
+//  SwiftBooks
+//
+//  Created by Libranner Leonel Santos Espinal on 14/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
